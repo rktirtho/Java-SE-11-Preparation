3eb8c712af2bc4864ce1b1c0dca5cf206db273cb
Rejaul Karim Tirtho <rktirtho@gmail.com>
2021-03-04 23:38:11 +0600

    variable declation

--- Java Operatiors/src/Test.java
+++ Java Operatiors/src/Test.java
@@ -1,7 +1,41 @@
+/**
+ * doc 1
+ */
 public class Test {
+
+
+
+    private String name = "Shally";
+
+    {
+
+        System.out.println(name);
+        name = "Lopez";
+        System.out.println(name);
+    }
+
+    public Test(){
+        name = "tiny";
+        email = "tiny@tiny.me";
+        System.out.println(name);
+        System.out.println("initializing consturctor");
+
+    }
+
     public static void main(String[] args) {
+
+        Test test = new Test();
+
+        int __=10;
+        System.out.println(__);
 
         String $df = "Java 11 Successfully installed";
         System.out.println($df);
+    }
+
+    String email;
+
+    {
+        System.out.println("end of class");
     }
 }
