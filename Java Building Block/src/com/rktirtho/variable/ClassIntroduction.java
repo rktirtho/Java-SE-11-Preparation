72b6ac5e2fe28ac6f2159ae5f27e16574163e797
Rejaul Karim Tirtho <rktirtho@gmail.com>
2021-03-13 22:49:33 +0600

    class variable introduction

---
+++ Java Building Block/src/com/rktirtho/variable/ClassIntroduction.java
@@ -0,0 +1,50 @@
+package com.rktirtho.variable;
+
+public class ClassIntroduction {
+	
+	private String firstName;		// Instance Variable
+	private String lastName;		// Instance Variable
+	
+	// Block of Code
+	// This block will execut before constructor body execution
+	{
+		
+		System.out.println("This block will execut before constructor body execution");
+	
+	}
+	
+	
+	// Constructor
+	public ClassIntroduction(String firstName, String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+
+	// Method
+	public String getFirstName() {
+		return firstName;
+	}
+
+	// Method
+	public void setFirstName(String firstName /*Method Parameter*/) {
+		this.firstName = firstName;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
+	
+	
+	
+
+}
