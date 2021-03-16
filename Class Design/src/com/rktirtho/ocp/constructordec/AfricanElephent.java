ba730ade3c3571a96aff04de6621caee8f016bd3
Rejaul Karim Tirtho <rktirtho@gmail.com>
2021-03-16 16:54:06 +0600

    constructor and inheritance example

--- Class Design/src/com/rktirtho/ocp/constructordec/AfricanElephent.java
+++ Class Design/src/com/rktirtho/ocp/constructordec/AfricanElephent.java
@@ -1,5 +1,7 @@
 package com.rktirtho.ocp.constructordec;
 
+import java.io.IOException;
+
 public class AfricanElephent extends Elephent{
 	public AfricanElephent() {
 		super(30, "");
@@ -7,6 +9,12 @@ public class AfricanElephent extends Elephent{
 	public AfricanElephent(int a) {
 		super(a, "");
 		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public void printInfo(){
+		// TODO Auto-generated method stub
+		super.printInfo();
 	}
 
 }
--- Class Design/src/com/rktirtho/ocp/constructordec/Elephent.java
+++ Class Design/src/com/rktirtho/ocp/constructordec/Elephent.java
@@ -11,5 +11,7 @@ public class Elephent extends Mammal{
 		super(a);
 		this.name =name;
 	}
+	
+	public void printInfo() {}
 
 }
