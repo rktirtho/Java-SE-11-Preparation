83b963398bc5c7ee83794963bd7a15a99913f850
Rejaul Karim Tirtho <rktirtho@gmail.com>
2021-03-22 22:28:56 +0600

    modular in java

--- Exception/src/com/rktirtho/ocp/exception/basics/Main.java
+++ Exception/src/com/rktirtho/ocp/exception/basics/Main.java
@@ -1,21 +1,31 @@
 package com.rktirtho.ocp.exception.basics;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		float s= 2/30;
+//		float s= 2/30;
 		try {
-			fall(5);
+			fall(58);
 			throw new ArrayIndexOutOfBoundsException();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			System.out.println(e.getMessage());
 		}catch (ArrayIndexOutOfBoundsException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+//			e.printStackTrace();
 		}
+		try {
+			Scanner scanner = new Scanner("");
+		}finally {
+			
+			
+		}
+		
+		var s=20;
+		System.out.println("Has no esd");
 	}
 
 	static public void fall(int distance) throws IOException {
