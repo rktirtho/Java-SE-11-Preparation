8323ea13d6b16c7fc822a1039b3be5da9e9c8c96
Rejaul Karim Tirtho <rktirtho@gmail.com>
2021-03-23 16:39:02 +0600

    creating inner class object

--- sec 2 java fandamenta;s/src/com/rktirtho/ocp/innerclasse/Main.java
+++ sec 2 java fandamenta;s/src/com/rktirtho/ocp/innerclasse/Main.java
@@ -1,5 +1,17 @@
 package com.rktirtho.ocp.innerclasse;
 
+import com.rktirtho.ocp.innerclasse.Student.Address;
+
 public class Main {
+	public static void main(String[] args) {
+		
+		Student student = new Student();
+		
+		Address address = student.new Address();
+		Address address3 = student.getAddress();
+		
+		
+		
+	}
 
 }
--- sec 2 java fandamenta;s/src/com/rktirtho/ocp/innerclasse/Student.java
+++ sec 2 java fandamenta;s/src/com/rktirtho/ocp/innerclasse/Student.java
@@ -1,10 +1,29 @@
 package com.rktirtho.ocp.innerclasse;
 
 public class Student {
-
-	public void name() {
-		class Address{
-			
+	
+	private String classId= "F565";
+	public Student() {
+		System.out.println("Student created...");
+	
+	}
+	
+	public Address getAddress() {
+		return new Address();
+	}
+	public class Address {
+		{
+			// inner class can access outclass private field
+			System.out.println(classId);
 		}
+		
+		public Address() {
+			System.out.println("Affress Created");
+		}
+		
+		void getAddress() {
+			System.out.println("Get Address");
+		}
+		
 	}
 }
