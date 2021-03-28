c470156c4b2e54628c59c6161893d7892826210e
Rejaul Karim Tirtho <rktirtho@gmail.com>
2021-03-29 00:39:15 +0600

    proxy

--- NIO.2/src/com/rktirtho/ocp/rw_with_buffered/ReadingAndWritingWithBuffered.java
+++ NIO.2/src/com/rktirtho/ocp/rw_with_buffered/ReadingAndWritingWithBuffered.java
@@ -1,13 +1,25 @@
 package com.rktirtho.ocp.rw_with_buffered;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class ReadingAndWritingWithBuffered {
 	public static void main(String[] args) {
-		tyr(var reader = Files.newBufferedReader(Path.of("d"))){
+		try {
+			System.out.println(Files.size(Path.of("/home/rktirtho/java-io")));
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try(var reader = Files.newBufferedReader(Path.of("/home/rktirtho/jva-io"))){
 			
+			String lines ;
+			
+			while((lines=reader.readLine())!=null) {
+				
+			}
 		}catch (Exception e) {
 			// TODO: handle exception
 		}
