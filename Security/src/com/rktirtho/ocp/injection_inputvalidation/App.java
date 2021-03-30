eee8903a6bcc22f0963c74ab64d3efd4caa22bee
Rejaul Karim Tirtho <rktirtho@gmail.com>
2021-03-30 23:24:20 +0600

    working with confidential data

--- Security/src/com/rktirtho/ocp/injection_inputvalidation/App.java
+++ Security/src/com/rktirtho/ocp/injection_inputvalidation/App.java
@@ -1,5 +1,14 @@
 package com.rktirtho.ocp.injection_inputvalidation;
 
-public class App {
+import java.util.Arrays;
 
+public class App {
+public static void main(String[] args) {
+	int arr[] = {4,74,9};
+	
+	Arrays.fill(arr, 'c');
+	for (int i = 0; i < arr.length; i++) {
+		System.out.println(arr[i]);
+	}
+}
 }
