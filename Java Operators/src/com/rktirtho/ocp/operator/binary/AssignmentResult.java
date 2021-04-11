ee746bd84687459d08a613f9724212e0048270a2
Rejaul Karim Tirtho <rktirtho@gmail.com>
2021-04-11 23:22:12 +0600

    revision

---
+++ Java Operators/src/com/rktirtho/ocp/operator/binary/AssignmentResult.java
@@ -0,0 +1,10 @@
+package com.rktirtho.ocp.operator.binary;
+
+public class AssignmentResult {
+public static void main(String[] args) {
+	int birds =50;
+	int awol = birds=30;
+	System.out.println(awol);
+	System.out.println(birds);
+}
+}
---
+++ Java Operators/src/com/rktirtho/ocp/operator/binary/Mdulas.java
@@ -0,0 +1,25 @@
+package com.rktirtho.ocp.operator.binary;
+
+import java.util.Iterator;
+
+public class Mdulas {
+
+	public static void main(String[] args) {
+		int a=-20;
+		int b =-3;
+		int c = a%b;
+		System.out.println(c);
+		
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i%5);
+			
+		}
+		
+		int sheep = 10;
+		long goat = 30L;
+		sheep*=goat;
+
+	}
+
+}
